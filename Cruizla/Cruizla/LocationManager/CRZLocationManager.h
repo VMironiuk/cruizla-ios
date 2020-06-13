b2944546966bf40c3d740d4382d9b1329ecc1e92
Vladimir Mironiuk <vladimironiuk@gmail.com>
2020-06-13 13:43:23 +0300

    Add CRZLocationManager

---
+++ Cruizla/Cruizla/LocationManager/CRZLocationManager.h
@@ -0,0 +1,22 @@
+//
+//  CRZLocationManager.h
+//  Cruizla
+//
+//  Created by Vladimir Mironiuk on 13.06.2020.
+//  Copyright © 2020 Vladimir Mironiuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CRZLocationManager : NSObject
+
++ (instancetype)sharedManager;
+
+- (void)start;
+- (void)stop;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Cruizla/Cruizla/SceneDelegate.swift
+++ Cruizla/Cruizla/SceneDelegate.swift
@@ -55,11 +55,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // Called when the scene has moved from an inactive state to an active state.
     // Use this method to restart any tasks that were paused (or not yet started) when the scene
     // was inactive.
+    CRZLocationManager.shared().start()
   }
   
   func sceneWillResignActive(_ scene: UIScene) {
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
+    CRZLocationManager.shared().stop()
   }
   
   func sceneWillEnterForeground(_ scene: UIScene) {
